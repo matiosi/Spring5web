ca97a5258139ebf6e26a857145ecd94e3826f431
mateusz.osiak <mateusz.osiak@lingaro.com>
2017-09-19 13:26:06 +0200

    commit 1

--- src/main/java/com/example/demo/model/Author.java
+++ src/main/java/com/example/demo/model/Author.java
@@ -17,7 +17,7 @@ public class Author {
     private String firstName;
     private String lastName;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books =new HashSet<>();
 
     public Author() {
@@ -64,5 +64,31 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
